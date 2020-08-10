3c0f7cb6620775ec7c5239cbfd8ce1427bc6f3ad
15857182156 <1004376731@qq.com>
2020-08-10 23:10:17 +0800

    update0810

--- showresults.py
+++ showresults.py
@@ -23,3 +23,26 @@ def show_results(img, results):
         img = cv2.putText(img, str(conf), (xmin, ymin), cv2.FONT_HERSHEY_COMPLEX, 2.0, (100, 200, 200), 5)
     cv2.imshow(' ', img)
     cv2.waitKey()
+
+
+def show_results1(img, results):
+    h, w = img.shape[:2]
+    tmpresults = []
+    wscale, hscale = w/448, h/448
+    for i in range(7):
+        for j in range(7):
+            if results[0, i, j, 0] > 0.2:
+                tmpresults.append(results[0, i, j, :])
+    for result in tmpresults:
+        conf, xmin, ymin, xmax, ymax = result.cpu().detach().numpy()
+        w, h = xmax - xmin, ymax - ymin
+        w, h = w * 448 * wscale, h * 448 * hscale
+        xcenter, ycenter = (xmin + xmax)/2, (ymin + ymax)/2
+        xcenter, ycenter = xcenter * 448 * wscale, ycenter * 448 * hscale
+        xmin, ymin = int(xcenter - w / 2), int(ycenter - h / 2)
+        xmax, ymax = int(xcenter + w / 2), int(ycenter + h / 2)
+        img = cv2.rectangle(img, (xmin, ymin), (xmax, ymax), (0, 255, 255), 2)
+        print(xmin, ymin, xmax, ymax)
+        img = cv2.putText(img, str(conf), (xmin, ymin), cv2.FONT_HERSHEY_COMPLEX, 2.0, (100, 200, 200), 5)
+    cv2.imshow(' ', img)
+    cv2.waitKey()
